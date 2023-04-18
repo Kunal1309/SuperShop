9675dede33ffe7d228942e3e2ed02f7b2d751ffd
Kunal1309 <titarekunal@gmail.com>
2023-04-18 22:27:32 +0530

    SuperShop

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div style={{width:"50%", backgroundColor:"goldenrod", color:"blue", padding:"4%", margin:"5% 0% 5% 25%"}}>
+        <h5>It was my first Major ReactJS Project.</h5>
+        <h5>
+          It was completed with the help of useState, useEffect & useContext
+          Hooks
+        </h5>
+        <h5>
+          In this project, all functions are working properly. You can use it!
+        </h5>
+        <i style={{marginLeft:"60%"}}>- Kunal Titare</i>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
